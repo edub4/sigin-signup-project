0da0aeb80b2a6fc123fb7bc4f41329e853411b66
eduardo santos <edusantos160506@gmail.com>
2024-10-21 21:45:57 -0300

    ts-tsx

---
+++ src/components/screen/input/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { container } from './styles';
+import { PropsWithChildren } from 'react';
+import { TextInput, TextInputProps } from "react-native";
+
+export const Input:React.FunctionComponent<TextInputProps> = ({... otherProps}: PropsWithchildren <TextInputProps>)
